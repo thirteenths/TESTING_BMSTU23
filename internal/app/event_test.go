ac859e943bca0c9c3be7f074f0e4b728452c3186
thirteenths <vera.korotkaya@yandex.ru>
2024-01-30 15:29:56 +0300

    add service test

--- internal/app/event_test.go
+++ internal/app/event_test.go
@@ -15,6 +15,7 @@ import (
 	"github.com/thirteenths/test-bmstu23/internal/domain"
 	"github.com/thirteenths/test-bmstu23/internal/domain/requests"
 	"github.com/thirteenths/test-bmstu23/internal/domain/responses"
+	"github.com/thirteenths/test-bmstu23/internal/infra/builder"
 	mock_storage "github.com/thirteenths/test-bmstu23/internal/infra/mock"
 )
 
@@ -45,7 +46,7 @@ func (suite *EventServiceTestSuite) TestGetAllEvent(t provider.T) {
 	ctx := context.Background()
 	testCase := map[int]struct {
 		nameTest         string
-		request          []domain.Event
+		mockResponse     []domain.Event
 		expectedResponse *responses.GetAllEvent
 		expectedError    error
 	}{
@@ -54,32 +55,39 @@ func (suite *EventServiceTestSuite) TestGetAllEvent(t provider.T) {
 			expectedResponse: &responses.GetAllEvent{
 				Event: []responses.Event{
 					{
-						ID:          1,
-						Name:        "testAll",
-						Description: "testAbout",
-						Date:        time.Time{},
+						ID:   1,
+						Name: "Big Stand Up",
+						Description: "Big Stand Up — шоу с самым большим процентом смеющихся людей. Здесь только опытные комики и шутки, проверенные не одной сотней избирательных зрителей." +
+							" Приходите убедиться в пятницу, субботу и воскресенье, если вам больше 18 лет и вы свободны в пятницу, субботу и воскресенье.",
+						Date: time.Time{},
+					},
+					{
+						ID:   2,
+						Name: "Жёсткий стендап",
+						Description: "Жёсткий стендап — это шоу, где комики могут шутить обо всем, о чем хотят, не боясь, что их сочтут сумасшедшими. " +
+							"А зрители могут смеяться над всем, над чем хотят, не боясь, что это неуместно. Точно будут шутки про ХХX, не*******ю и к****c. " +
+							"В шоу участвуют 4 комика и ведущий. Состав обновляется раз в месяц — успеете попасть на любимого стендапера. Приходите на шоу, чтобы посмеяться без стыда и, " +
+							"возможно, расширить границы дозволенного. 18+",
+						Date: time.Time{},
 					},
 				},
 			},
-			request: []domain.Event{
-				{
-					ID:          1,
-					Name:        "testAll",
-					Description: "testAbout",
-					Date:        time.Time{},
-				}},
+			mockResponse: []domain.Event{
+				*builder.EventMother{}.Obj1(),
+				*builder.EventMother{}.Obj2(),
+			},
 			expectedError: nil,
 		},
 		2: {
 			nameTest:         "Test Error",
 			expectedResponse: nil,
-			request:          []domain.Event{},
+			mockResponse:     nil,
 			expectedError:    errors.New("storage error")},
 	}
 
 	for _, test := range testCase {
 		// Mock the storage method call
-		suite.mockStorage.EXPECT().GetAllEvent(ctx).Return(test.request, test.expectedError)
+		suite.mockStorage.EXPECT().GetAllEvent(ctx).Return(test.mockResponse, test.expectedError)
 
 		// Call the service method
 		actualResponse, actualError := suite.eventService.GetAllEvent(ctx)
@@ -94,36 +102,32 @@ func (suite *EventServiceTestSuite) TestGetEvent(t provider.T) {
 	ctx := context.Background()
 	testCase := map[int]struct {
 		nameTest         string
-		request          domain.Event
+		mockResponse     domain.Event
 		expectedResponse *responses.GetEvent
 		expectedError    error
 	}{
 		1: {
 			nameTest: "Test Ok",
 			expectedResponse: &responses.GetEvent{
-				ID:          1,
-				Name:        "testAll",
-				Description: "testAbout",
-				Date:        time.Time{},
+				ID:   1,
+				Name: "Big Stand Up",
+				Description: "Big Stand Up — шоу с самым большим процентом смеющихся людей. Здесь только опытные комики и шутки, проверенные не одной сотней избирательных зрителей." +
+					" Приходите убедиться в пятницу, субботу и воскресенье, если вам больше 18 лет и вы свободны в пятницу, субботу и воскресенье.",
+				Date: time.Time{},
 			},
-			request: domain.Event{
-				ID:          1,
-				Name:        "testAll",
-				Description: "testAbout",
-				Date:        time.Time{},
-			},
+			mockResponse:  *builder.EventMother{}.Obj1(),
 			expectedError: nil,
 		},
 		2: {
 			nameTest:         "Test Error",
 			expectedResponse: nil,
-			request:          domain.Event{},
+			mockResponse:     domain.Event{},
 			expectedError:    errors.New("storage error")},
 	}
 
 	for _, test := range testCase {
 		// Mock the storage method call
-		suite.mockStorage.EXPECT().GetEvent(ctx, 1).Return(test.request, test.expectedError)
+		suite.mockStorage.EXPECT().GetEvent(ctx, 1).Return(test.mockResponse, test.expectedError)
 
 		// Call the service method
 		actualResponse, actualError := suite.eventService.GetEvent(ctx, 1)
@@ -137,42 +141,39 @@ func (suite *EventServiceTestSuite) TestGetEvent(t provider.T) {
 func (suite *EventServiceTestSuite) TestCreateEvent(t provider.T) {
 	ctx := context.Background()
 	testCase := map[int]struct {
-		nameTest         string
-		request          int
-		expectedResponse *requests.CreateEvent
-		expect           *domain.Event
-		expectedError    error
+		nameTest      string
+		request       int
+		mockRequest   *requests.CreateEvent
+		mockResponse  *domain.Event
+		expectedError error
 	}{
 		1: {
 			nameTest: "Test Ok",
-			expectedResponse: &requests.CreateEvent{
-				Name:        "testAll",
-				Description: "testAbout",
-				Date:        time.Time{},
+			mockRequest: &requests.CreateEvent{
+				Name: "Big Stand Up",
+				Description: "Big Stand Up — шоу с самым большим процентом смеющихся людей. Здесь только опытные комики и шутки, проверенные не одной сотней избирательных зрителей." +
+					" Приходите убедиться в пятницу, субботу и воскресенье, если вам больше 18 лет и вы свободны в пятницу, субботу и воскресенье.",
+				Date: time.Time{},
 			},
-			expect: &domain.Event{
-				ID:          0,
-				Name:        "testAll",
-				Description: "testAbout",
-				Date:        time.Time{},
-			},
+			mockResponse:  builder.EventMother{}.Obj1(),
 			request:       1,
 			expectedError: nil,
 		},
 		2: {
-			nameTest:         "Test Error",
-			expectedResponse: nil,
-			expect:           nil,
-			request:          0,
-			expectedError:    errors.New("storage error")},
+			nameTest:      "Test Error",
+			mockRequest:   nil,
+			mockResponse:  nil,
+			request:       0,
+			expectedError: errors.New("storage error")},
 	}
-
+	testCase[1].mockResponse.ID = 0
 	for _, test := range testCase {
+
 		// Mock the storage method call
-		suite.mockStorage.EXPECT().CreateEvent(ctx, *test.expect).Return(test.request, test.expectedError)
+		suite.mockStorage.EXPECT().CreateEvent(ctx, *test.mockResponse).Return(test.request, test.expectedError)
 
 		// Call the service method
-		actualResponse, actualError := suite.eventService.CreateEvent(ctx, *test.expectedResponse)
+		actualResponse, actualError := suite.eventService.CreateEvent(ctx, *test.mockRequest)
 
 		// Compare the expected and actual responses
 		t.Require().Equal(test.expectedError, actualError)
@@ -183,72 +184,54 @@ func (suite *EventServiceTestSuite) TestCreateEvent(t provider.T) {
 func (suite *EventServiceTestSuite) TestUpdateEvent(t provider.T) {
 	ctx := context.Background()
 	testCase := map[int]struct {
-		nameTest         string
-		request          int
-		expectedResponse *requests.UpdateEvent
-		expect           *domain.Event
-		expectedError    error
+		nameTest      string
+		mockRequest   *requests.UpdateEvent
+		mockResponse  *domain.Event
+		expectedError error
 	}{
 		1: {
 			nameTest: "Test Ok",
-			expectedResponse: &requests.UpdateEvent{
-				Name:        "testAll",
-				Description: "testAbout",
-				Date:        time.Time{},
+			mockRequest: &requests.UpdateEvent{
+				Name: "Big Stand Up",
+				Description: "Big Stand Up — шоу с самым большим процентом смеющихся людей. Здесь только опытные комики и шутки, проверенные не одной сотней избирательных зрителей." +
+					" Приходите убедиться в пятницу, субботу и воскресенье, если вам больше 18 лет и вы свободны в пятницу, субботу и воскресенье.",
+				Date: time.Time{},
 			},
-			expect: &domain.Event{
-				ID:          0,
-				Name:        "testAll",
-				Description: "testAbout",
-				Date:        time.Time{},
-			},
-			request:       1,
+			mockResponse:  builder.EventMother{}.Obj1(),
 			expectedError: nil,
 		},
 		2: {
-			nameTest:         "Test Error",
-			expectedResponse: nil,
-			expect:           nil,
-			request:          0,
-			expectedError:    errors.New("storage error")},
+			nameTest:      "Test Error",
+			mockRequest:   nil,
+			mockResponse:  nil,
+			expectedError: errors.New("storage error")},
 	}
-
+	testCase[1].mockResponse.ID = 0
 	for _, test := range testCase {
 		// Mock the storage method call
-		suite.mockStorage.EXPECT().UpdateEvent(ctx, *test.expect, 1).Return(test.expectedError)
+		suite.mockStorage.EXPECT().UpdateEvent(ctx, *test.mockResponse, 1).Return(test.expectedError)
 
 		// Call the service method
-		actualError := suite.eventService.UpdateEvent(ctx, *test.expectedResponse, 1)
+		actualError := suite.eventService.UpdateEvent(ctx, *test.mockRequest, 1)
 
 		// Compare the expected and actual responses
 		t.Require().Equal(test.expectedError, actualError)
-		// t.Require().True(reflect.DeepEqual(test.expectedResponse, actualResponse))
 	}
 }
 
 func (suite *EventServiceTestSuite) TestDeleteEvent(t provider.T) {
 	ctx := context.Background()
 	testCase := map[int]struct {
-		nameTest         string
-		request          int
-		expectedResponse *requests.CreateEvent
-		expectedError    error
+		nameTest      string
+		expectedError error
 	}{
 		1: {
-			nameTest: "Test Ok",
-			expectedResponse: &requests.CreateEvent{
-				Name:        "testAll",
-				Description: "testAbout",
-				Date:        time.Time{},
-			},
-			request:       1,
+			nameTest:      "Test Ok",
 			expectedError: nil,
 		},
 		2: {
-			nameTest:         "Test Error",
-			expectedResponse: nil,
-			request:          0,
-			expectedError:    errors.New("storage error")},
+			nameTest:      "Test Error",
+			expectedError: errors.New("storage error")},
 	}
 
 	for _, test := range testCase {
@@ -260,7 +243,6 @@ func (suite *EventServiceTestSuite) TestDeleteEvent(t provider.T) {
 
 		// Compare the expected and actual responses
 		t.Require().Equal(test.expectedError, actualError)
-		// t.Require().True(reflect.DeepEqual(test.expectedResponse, actualResponse))
 	}
 }
 
