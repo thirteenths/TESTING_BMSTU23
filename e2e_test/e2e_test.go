bf0d4ace23be85e026ad84dd3a362b4b19d01d08
thirteenths <vera.korotkaya@yandex.ru>
2024-02-02 15:03:28 +0300

    add e2e

--- e2e_test/e2e_test.go
+++ e2e_test/e2e_test.go
@@ -1,9 +1,29 @@
 package e2e_test
 
 import (
+	"database/sql"
+	"embed"
+	"github.com/pressly/goose/v3"
+	log "github.com/sirupsen/logrus"
 	"testing"
 )
 
+var db *sql.DB
+
+//go:embed migrations/*.sql
+var embedMigrations embed.FS
+
 func TestMain(m *testing.M) {
-	
+	goose.SetBaseFS(embedMigrations)
+
+	err := goose.SetDialect("postgres")
+	if err != nil {
+		panic(err)
+	}
+	db, err = sql.Open("postgres", "postgres://postgres:7dgvJVDJvh254aqOpfd@localhost:5432/postgres?sslmode=disable")
+	if err != nil {
+		log.Fatalf("Could not connected: %s", err)
+	}
+
+	m.Run()
 }
