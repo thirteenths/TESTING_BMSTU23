4f36f19394987532525d1d99a4aa50356c3914be
thirteenths <vera.korotkaya@yandex.ru>
2024-02-02 17:49:07 +0300

    add e2e

--- e2e_test/get-event_test.go
+++ e2e_test/get-event_test.go
@@ -5,23 +5,42 @@ import (
 	"fmt"
 	"github.com/pressly/goose/v3"
 	log "github.com/sirupsen/logrus"
-	"net/http"
 	"testing"
+
+	"github.com/ozontech/allure-go/pkg/framework/provider"
+	"github.com/ozontech/allure-go/pkg/framework/suite"
+
+	"net/http"
 )
 
-func TestGetEvent(t *testing.T) {
+type GetEventEnd2EndTestSuite struct {
+	suite.Suite
+}
+
+func (suite *GetEventEnd2EndTestSuite) BeforeAll(t provider.T) {
 	err := goose.UpTo(db, "migrations", 20240117165259)
 	if err != nil {
 		log.Warnf("Error migration: %s", err)
 	}
+}
 
-	resp, err := http.Get("http://localhost:5000/bmstu-stud-web/api/events/")
+func (suite *GetEventEnd2EndTestSuite) AfterAll(t provider.T) {
+	err := goose.DownTo(db, "migrations", 20240117165259)
+	if err != nil {
+		log.Warnf("Error migration: %s", err)
+	}
+}
+
+func TestEndToEnd_GetEvent(t *testing.T) {
+	suite.RunSuite(t, new(GetEventEnd2EndTestSuite))
+}
+
+func (suite *GetEventEnd2EndTestSuite) TestGetEvent(t provider.T) {
+	resp, err := http.Get("http://localhost:5000/bmstu-stud-web/api/events/1")
 	if err != nil {
 		panic(err)
 	}
 	defer resp.Body.Close()
-
-	fmt.Println("Response status:", resp.Status)
 
 	scanner := bufio.NewScanner(resp.Body)
 	for i := 0; scanner.Scan() && i < 5; i++ {
@@ -32,8 +51,6 @@ func TestGetEvent(t *testing.T) {
 		panic(err)
 	}
 
-	err = goose.DownTo(db, "migrations", 20240117165259)
-	if err != nil {
-		log.Warnf("Error migration: %s", err)
-	}
+	t.Require().NotNil(resp.Body)
+	t.Require().Equal(resp.StatusCode, 200)
 }
